5f8e5e0404ee10eaf5a50b1c5a84c50d5096182d
exlineo <contact@exlineo.com>
2019-10-22 19:56:20 +0200

    Modification de la démo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  REST_ADR: 'http://localhost:1337/',
+  // REST_ADR: 'http://localhost:1337/',
+  REST_ADR: '/assets/datas/',
   NG_NEWS:'nouvelles',
   NG_AUTEUR:'auteurs'
 };
